0eb02144462cf035290804eec47e949f42d57117
Itunu Raimi <chavitunu@gmail.com>
2021-03-02 15:28:21 +0100

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Itunu Raimi on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
