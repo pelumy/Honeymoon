36ac298af3ae6cb0375952d9a2d8dc452628443a
Itunu Raimi <chavitunu@gmail.com>
2021-03-03 15:43:39 +0100

    added gesture and animations to cardview

--- Honeymoon/View/CardView.swift
+++ Honeymoon/View/CardView.swift
@@ -7,9 +7,10 @@
 
 import SwiftUI
 
-struct CardView: View {
+struct CardView: View, Identifiable {
     // MARK: - PROPERTIES
     var honeymoon: Destination
+    let id =  UUID()
     
     // MARK: - BODY
     var body: some View {
@@ -57,7 +58,7 @@ struct CardView: View {
 // MARK: - PREVIEW
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView(honeymoon: honeymoonData[1])
+        CardView( honeymoon: honeymoonData[0])
             .previewLayout(.fixed(width: 375, height: 600))
     }
 }
